7591b369365b0042091cf2bfc9e8dfac92f8000f
nij4life <TjOdapQzPaswen88>
2020-10-13 15:46:02 +0300

    fix: change products>description type to text

--- db/schema.rb
+++ db/schema.rb
@@ -1,30 +1,42 @@
-ActiveRecord::Schema.define(version: 2020_10_13_120757) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.string 'url'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2020_10_13_124405) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'categories_products', id: false, force: :cascade do |t|
-    t.integer 'category_id', null: false
-    t.integer 'product_id', null: false
-    t.index ['category_id', 'product_id'], name: 'index_categories_products_on_category_id_and_product_id'
-    t.index ['product_id', 'category_id'], name: 'index_categories_products_on_product_id_and_category_id'
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.integer 'ad_id'
-    t.string 'name'
-    t.string 'url'
-    t.string 'city'
-    t.integer 'year'
-    t.decimal 'price'
-    t.string 'photo_url'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "products", force: :cascade do |t|
+    t.integer "ad_id"
+    t.string "name"
+    t.string "url"
+    t.string "city"
+    t.integer "year"
+    t.decimal "price"
+    t.string "photo_url"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
